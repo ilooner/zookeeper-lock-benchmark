0a06969d71764c42d5646f8a2f39f53a7c886dce
Timothy Farkas <timothyfarkas@apache.org>
2018-09-06 13:03:35 -0700

    - Got packaging and basic shell script working

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+bin=`dirname "${BASH_SOURCE-$0}"`
+bin=`cd -P "$bin">/dev/null; pwd`
+home="$(dirname "$bin")"
+jars=$home"/jars/*"
+
+echo $bin
+echo $jars
+echo $home
+
+java -cp "$jars" org.apache.bench.Cmd $@
+
