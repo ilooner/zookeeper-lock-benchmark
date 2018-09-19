3049ad932351745eb265781e8c422857e9197bd0
root <root@pssc-67.qa.lab>
2018-09-19 16:05:24 -0700

    Fixing the python script to generate io-ops

--- scripts/parse-log.py
+++ scripts/parse-log.py
@@ -27,10 +27,16 @@ def get_all_files_in_dir(path):
 def write(file, dictionary):
     with open(file, 'w') as target:
         writer=csv.writer(target, delimiter='\n', quoting = csv.QUOTE_NONE,escapechar='\\')
-        for key, value in d.iteritems():
+        for key, value in dictionary.iteritems():
             writer.writerow([key.replace('^.*Results-Aggregated-','')])
             writer.writerow(value)
     
+def writefinal(file, dictionary):
+    with open(file, 'w') as target:
+        writer=csv.writer(target, delimiter=',', quoting = csv.QUOTE_NONE,escapechar='\\')
+        for key, value in dictionary.iteritems():
+            print value
+            writer.writerow(value)
 
 if len(sys.argv) > 2 or len(sys.argv) <= 1:
    print "Please provide directory as the input!!"
@@ -40,13 +46,51 @@ if not os.path.isdir(sys.argv[1]):
    print "Provided input is not a directory!!!"
    sys.exit()
 
+if os.path.isfile(sys.argv[1]+'/final-results') and os.access(sys.argv[1]+'/final-results', os.R_OK):
+    os.remove(sys.argv[1]+'/final-results')
+
+if os.path.isfile(sys.argv[1]+'/iops-output') and os.access(sys.argv[1]+'/iops-output', os.R_OK):
+    os.remove(sys.argv[1]+'/iops-output')
+
 d = dict()
 for f in get_all_files_in_dir(sys.argv[1]):
    (test, testname) = get_test_name(f)
    for line in aggregate_finder(f):
        (key, value) = line.split(':')
        if key in d:
-           d[key].append(clean(testname) + ':' + clean(value))
+           d[key].append(clean(testname) + ',' + clean(value))
        else:
-           d[key] = [clean(testname) + ':' + clean(value)]
+           d[key] = [clean(testname) + ',' + clean(value)]
+
 write(sys.argv[1]+'/final-results', d)
+testnamedict = dict()
+for key, value in d.iteritems():
+    results = [x.split(',') for x in value]
+    for item in results:
+        if item[0] in testnamedict:
+            testnamedict[key+item[0]].append(item)
+        else:
+            testnamedict[key+item[0]] = [item]
+#print testnamedict
+output = dict()
+for key, value in testnamedict.iteritems():
+    output[key] = [sum([int(x[1]) for x in value]), max([int(x[3]) for x in value])]
+#print output
+
+total = dict()
+for key, value in output.iteritems():
+    item = key.split(' ')[0][:4]+key.split(' ')[1]
+    if item in total:
+        total[item].append(value)
+    else:
+        total[item] = [value]
+#print total
+
+final = dict()
+for key, value in total.iteritems():
+    totaltimeinsec = sum(int(x[1]) for x in value)/1000
+    if totaltimeinsec==0:
+        final[key] = [clean(key), 0]
+    else:
+        final[key] = [clean(key), min(int(x[0]) for x in value) / totaltimeinsec]
+writefinal(sys.argv[1]+'/iops-output', final)
